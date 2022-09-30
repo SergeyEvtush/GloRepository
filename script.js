d244b637c117202a9449e90865eea1e352a210f7
Sergey Evtushenko <evtushru208@gmail.com>
2022-10-01 01:46:00 +0200

    создал переменные ,вывел alert,вывел в консоль текст

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const title = '';
+const screen = '';
+const screenPrice = 0;
+const rollbac = true;
+const fullPric = 0;
+const adaptiv = true;
+alert('hello teacher');
+console.log('some text');
